9e1b715fd26937ac70f3af57e2685ef4029816ca
Ed Ronda <edgar.g.ronda@gmail.com>
2019-06-16 18:04:13 -0700

    Adding MomentJS in order to have beauty dates.

--- CIOL/App_Start/BundleConfig.cs
+++ CIOL/App_Start/BundleConfig.cs
@@ -20,7 +20,9 @@ namespace CIOL
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/moment.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
