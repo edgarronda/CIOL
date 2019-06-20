bec31dc17b458e6731b5d4f9ec83c2e85a1ede03
Ed Ronda <edgar.g.ronda@gmail.com>
2019-06-20 15:55:20 -0700

    Fix UI libraries.

--- CIOL/App_Start/BundleConfig.cs
+++ CIOL/App_Start/BundleConfig.cs
@@ -11,27 +11,22 @@ namespace CIOL
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-3.4.1.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-2.8.3"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js",
                       "~/Scripts/moment.js"
                       ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/site.css",
-                       "~/Content/datatables.min.css"));
-
             bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
-                   "~/Scripts/datatables.min.js",
+                   "~/Scripts/jquery.dataTables.min.js",
+                   "~/Scripts/dataTables.bootstrap4.min.js",
                    "~/Scripts/buttons.html5.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/Content/bootstrap.min.css",
+                    "~/Content/jquery.dataTables.min.css",                                         
+                    "~/Content/dataTables.bootstrap4.min.css",
+                    "~/Content/site.css"));
+
         }
     }
 }
